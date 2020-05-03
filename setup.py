5052ef5674c41681755b2b9a3e1300ed84a31aef
Ben Nagel <bnagel@kinestral.com>
2020-05-03 14:18:30 -0600

    chore: bump version to v0.1.1
    Bump version to v0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="idenaTelegramUpdater",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=["Twisted>=17.9.0", "attrs", "python-telegram-bot", "treq"],
